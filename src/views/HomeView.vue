8e9318731c90151d18e8592b8c735406224bcc0f
Tran Xuan Duc <xduc24122002@gmail.com>
2024-03-27 14:35:06 +0700

    init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <main>
+    <h1 class="green">Xin chao {{ name }}</h1>
+    <TheWelcome />
+  </main>
+</template>
