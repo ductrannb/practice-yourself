f6407afa318bb8130024cfddb40612d1104e332e
Tran Xuan Duc <xduc24122002@gmail.com>
2024-03-27 16:17:21 +0700

    test new job

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">{{ msg }} Xin chao</h1>
     <h3>
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
