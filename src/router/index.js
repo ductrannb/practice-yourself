c5328022ee5c0e4c09ca050b22a441f696a16712
Tran Xuan Duc <xduc24122002@gmail.com>
2024-03-29 15:05:03 +0700

    add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import HelloWorld from "@/views/HelloWorld.vue";
+import PageNotFound from "@/views/PageNotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/hello',
       name: 'hello',
       component: HelloWorld
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: PageNotFound
     }
   ]
 })
