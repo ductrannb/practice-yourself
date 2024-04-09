653260dc5fce763a92ee62d1a380132fb0be9de1
Tran Xuan Duc <xduc24122002@gmail.com>
2024-04-09 15:57:44 +0700

    google one tap

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,11 +3,21 @@
 </template>
 
 <script>
-import {GoogleLogin} from "vue3-google-login";
+import { GoogleLogin } from "vue3-google-login"
+import { googleOneTap } from "vue3-google-login"
 
 export default {
   name: "Login",
   components: {GoogleLogin},
+  mounted() {
+    googleOneTap()
+      .then((response) => {
+        console.log("Handle the response", response)
+      })
+      .catch((error) => {
+        console.log("Handle the error", error)
+      })
+  },
   methods: {
     googleLogin(response) {
       console.log(response)
