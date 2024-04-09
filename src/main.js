56b5f3bdb32e8c7fb9dcddcd3ba1efa6540d7b15
Tran Xuan Duc <xduc24122002@gmail.com>
2024-04-09 15:52:58 +0700

    google login with package

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import mixin from "@/Utils/mixin.js"
 import axios from "@/plugins/axios.js"
 import vuetify from "@/plugins/vuetify.js"
 import './assets/main.css'
-import vuex from "@/plugins/vuex.js";
+import vuex from "@/plugins/vuex.js"
+import vue3GoogleLogin from 'vue3-google-login'
 
 window.axios = axios
 
 const app = createApp(App)
 
-app.use(router).use(vuetify).use(vuex)
+app.use(router).use(vuetify).use(vuex).use(vue3GoogleLogin, {
+    clientId: '509066628056-3acdojkep730qt997b9q070m8q59a2ai.apps.googleusercontent.com'
+})
 app.mixin(mixin)
 app.mount('#app')
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="g-signin2" data-onsuccess="onSignIn"></div>
+  <GoogleLogin :callback="googleLogin"/>
 </template>
 
 <script>
+import {GoogleLogin} from "vue3-google-login";
+
 export default {
   name: "Login",
+  components: {GoogleLogin},
   methods: {
-    onSignIn(googleUser) {
-      let profile = googleUser.getBasicProfile();
-      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-      console.log('Name: ' + profile.getName());
-      console.log('Image URL: ' + profile.getImageUrl());
-      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    googleLogin(response) {
+      console.log(response)
     }
   }
 }
