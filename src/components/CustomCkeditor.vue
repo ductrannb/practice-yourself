5ee40ab6b4cebbfcbed029e21845270b835df181
Tran Xuan Duc <xduc24122002@gmail.com>
2024-04-22 17:44:14 +0700

    add ui

---
+++ src/components/CustomCkeditor.vue
@@ -0,0 +1,24 @@
+<template>
+  <ckeditor :editor="editor" :config="editorConfig" v-model="model"></ckeditor>
+</template>
+
+<script>
+import CKEditor from "@ckeditor/ckeditor5-vue"
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+export default {
+  name: 'CustomCkeditor',
+  components: [CKEditor],
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorConfig: {
+      },
+      model: ''
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
