2737d222c6888dd136131134ef239150f7214907
Tran Xuan Duc <kiaisoft.duc.tran@gmail.com>
2024-05-16 23:14:12 +0700

    add ui

---
+++ src/components/Payment.vue
@@ -0,0 +1,43 @@
+<template>
+
+</template>
+
+<script>
+import { usePayOS, PayOSConfig } from "payos-checkout"
+export default {
+  name: "Payment",
+  data() {
+    return {
+      payOS: {}
+    }
+  },
+  mounted() {
+    this.initPayOS()
+  },
+  methods: {
+    initPayOS() {
+      const payOSConfig: PayOSConfig = {
+        RETURN_URL: '',
+        ELEMENT_ID: 'app',
+        CHECKOUT_URL: '',
+        onSuccess: (event: any) => {
+          this.$emit('onSuccess', event)
+        },
+        onCancel: (event: any) => {
+          this.$emit('onCancel', event)
+        },
+        onExit: (event: any) => {
+          this.$emit('onExit', event)
+        }
+      }
+      const payOS = usePayOS(payOSConfig)
+      this.payOS = payOS
+      payOS.open()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
